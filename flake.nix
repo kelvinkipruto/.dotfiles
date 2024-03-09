464a813035f7716f2568acdc0dc2079a48665b0e
kelvinkipruto <user@email.com>
2024-03-10 02:17:54 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My Dotfiles Flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+    };
+  };
+
+  outputs = {self, nixpkgs, ...}:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [./configuration.nix];.
+      };
+    };
+  };
+
+}
